02a0446efb3e9ce0569ffbace2b5bb7415c36886
RenarSiimon <renar.siimon@techno.ee>
2026-04-21 14:40:13 +0300

    add username to DB

---
+++ script.js
@@ -0,0 +1,43 @@
+const URL = "https://tinkr.tech/sdb/poly/wander";
+
+let playerKey = localStorage.getItem("playerKey");
+let myUsername = localStorage.getItem("myUsername");
+
+document.getElementById("join").addEventListener("click", async () => {
+    const username = document.getElementById("name").value.trim();
+
+    if (!username) return;
+
+    const res = await fetch(URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ action: "join", username })
+    });
+
+    const data = await res.json();
+
+    playerKey = data.player_key;
+    myUsername = username;
+
+    localStorage.setItem("playerKey", playerKey);
+    localStorage.setItem("myUsername", myUsername);
+});
+document.getElementById("say").addEventListener("click", async () => {
+    const message = document.getElementById("talk").value.trim();
+    if (!message || !playerKey) return;
+ 
+    await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ action: "talk", player_key: playerKey, message })
+    });
+    document.getElementById("talk").value = "";
+});
+
+document.getElementById("leave").addEventListener("click", () => {
+    localStorage.removeItem("playerKey");
+    localStorage.removeItem("myUsername");
+    playerKey = null;
+    myUsername = null;
+});
+
